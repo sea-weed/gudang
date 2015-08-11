1f70ed8df6bc2fbb0f8f9245cd54863397b76c0a
sea-weed <myseaweed@outlook.com>
2015-08-11 14:30:46 +0700

    mvn

---
+++ goodank/src/main/java/id/co/askrindo/gudang/App.java
@@ -0,0 +1,13 @@
+package id.co.askrindo.gudang;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
